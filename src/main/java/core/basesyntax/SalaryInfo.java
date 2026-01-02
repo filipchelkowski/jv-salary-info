9d8827a2a1dfd6089577f03ca3210313fa227fb3
filipchelkowski <filip.chelkowski@gmail.com>
2026-01-02 19:12:24 +0100

    Mvn clean test and pull request keep on giving different results

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,20 +4,23 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    private static final int i0 = 0;
+    private static final int in = 1;
+    private static final int ih = 2;
+    private static final int is = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         int[] salaryPerPerson = new int[names.length];
-        final int in = 1;
-        final int ih = 2;
-        final int is = 3;
 
-        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         LocalDate df = LocalDate.parse(dateFrom, formatter);
         LocalDate dt = LocalDate.parse(dateTo, formatter);
 
         for (String day : data) {
             String line = day.trim();
             String[] res = line.split("\s+");
-            LocalDate date = LocalDate.parse(res[0], formatter);
+            LocalDate date = LocalDate.parse(res[i0], formatter);
             if ((date.isEqual(df) || date.isAfter(df)) && (date.isEqual(dt) || date.isBefore(dt))) {
                 for (int i = 0; i < names.length; i++) {
                     if (res[in].equals(names[i])) {
