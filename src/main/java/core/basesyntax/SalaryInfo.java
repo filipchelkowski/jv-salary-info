6813d7034a9f6145b8d5674e3650605dad6e4392
filipchelkowski <filip.chelkowski@gmail.com>
2026-01-02 18:15:17 +0100

    Task solved

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,39 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        int[] salaryPerPerson = new int[names.length];
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate dF = LocalDate.parse(dateFrom, formatter);
+        LocalDate dT = LocalDate.parse(dateTo, formatter);
+
+        for (String day : data) {
+            String[] result = day.split(" ");
+            LocalDate date = LocalDate.parse(result[0], formatter);
+            if (date.isAfter(dF) && date.isBefore(dT) || date.isEqual(dF) || date.isEqual(dT)) {
+                for (int i = 0; i < names.length; i++) {
+                    if (result[1].equals(names[i])) {
+                        salaryPerPerson[i] = Integer.parseInt(result[2]) * Integer.parseInt(result[3]);
+                    }
+                }
+            }
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Report for period " + dateFrom + " - " + dateTo + "\n");
+
+        for (int i = 0; i < names.length; i++) {
+            stringBuilder.append(names[i])
+                    .append(" - ")
+                    .append(salaryPerPerson[i])
+                    .append("\n");
+        }
+
+        return stringBuilder.toString();
     }
+
 }
