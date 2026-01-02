ddc3d0b134a10a473282d95e6b23156011a8a1bc
filipchelkowski <filip.chelkowski@gmail.com>
2026-01-02 18:58:54 +0100

    Test

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,25 +8,37 @@ public class SalaryInfo {
         int[] salaryPerPerson = new int[names.length];
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate dF = LocalDate.parse(dateFrom, formatter);
-        LocalDate dT = LocalDate.parse(dateTo, formatter);
+        LocalDate df = LocalDate.parse(dateFrom, formatter);
+        LocalDate dt = LocalDate.parse(dateTo, formatter);
 
         for (String day : data) {
-            String[] result = day.split(" ");
-            LocalDate date = LocalDate.parse(result[0], formatter);
-            if (date.isAfter(dF) && date.isBefore(dT) || date.isEqual(dF) || date.isEqual(dT)) {
+            String line = day.trim();
+            String[] res = line.split("\s+");
+            LocalDate date = LocalDate.parse(res[0], formatter);
+            if ((date.isEqual(df) || date.isAfter(df)) && (date.isEqual(dt) || date.isBefore(dt))) {
                 for (int i = 0; i < names.length; i++) {
-                    if (result[1].equals(names[i])) {
-                        salaryPerPerson[i] = Integer.parseInt(result[2]) * Integer.parseInt(result[3]);
+                    if (res[1].equals(names[i])) {
+                        salaryPerPerson[i] += Integer.parseInt(res[2]) * Integer.parseInt(res[3]);
+                        break;
                     }
                 }
             }
         }
 
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Report for period " + dateFrom + " - " + dateTo + "\n");
+        stringBuilder.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo)
+                .append("\n");
 
         for (int i = 0; i < names.length; i++) {
+            if (i == names.length - 1) {
+                stringBuilder.append(names[i])
+                        .append(" - ")
+                        .append(salaryPerPerson[i]);
+                break;
+            }
             stringBuilder.append(names[i])
                     .append(" - ")
                     .append(salaryPerPerson[i])
